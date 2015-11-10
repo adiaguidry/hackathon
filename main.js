db3a017628627bf88bfdf931783f86232d5b7ac4
dnguyen81git <davidhuynguyen81@gmail.com>
2015-11-10 15:44:10 -0800

    changing click functionality, not done

--- main.js
+++ main.js
@@ -102,14 +102,14 @@ $(document).ready(function () {
     });
 
     //=============youtube search function with button====================//
-    $('#search').click(function () {
-        var searchResult = document.getElementById('query').value;
+    $('<a>').click(function () {
+        var searchResult = all_books_array[i];
 
         apis.youtube.getData(searchResult, '10', function (success, resp) {
             videoId = resp;
-            console.log("before success call", resp.video[1]);
+            console.log("before success call", resp.video[0]);
             if (success) {
-                apis.youtube.playVideo(resp.video[1].id, 195, 320);
+                apis.youtube.playVideo(resp.video[0].id, 195, 320);
                 console.log("success called", resp);
             }
         });
