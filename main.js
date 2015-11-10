eacf90b064a67e32b2fb39d8363c4766ae0f8e55
adiaguidry <adiaguidry@gmail.com>
2015-11-10 12:43:46 -0800

    made book objects

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 var goodreads_key = 'key: orU88aqllNy1ZoJLTTH9Q';
 var goodreads_secret = 'secret: 3fnOGoSE7vkY8s96FFeoppDlAiHXMGanqAORgCqwU8M';
 var global_result;
+var  book_object = function(title, author, summary){
+   var self = this;
+    this.title = title;
+    this.author = author;
+    this.summary = summary;
+};
+var all_books_array =[];
+
 
 $(document).ready(function () {
         $.ajax({
@@ -15,6 +23,7 @@ $(document).ready(function () {
                     var book_image = global_result.feed.entry[i]["im:image"][2].label;
                     var book_name = book_array[i]["im:name"].label;
                     var book_author = book_array[i]["im:artist"].label;
+                    var book_summary = book_array[i].summary.label;
                     var img_tag = $('<img>');
                     var a_tag = $('<a>');
                     var h3_tag = $('<h3>');
@@ -27,6 +36,10 @@ $(document).ready(function () {
                     $(img_tag).attr('src',book_image);
                     $('.book_name').html(book_name);
                     $('p').html(book_author);
+
+                   var book = new book_object(book_name, book_author, book_summary);
+                    all_books_array.push(book);
+                    console.log(all_books_array);
 
                     }
                 }
