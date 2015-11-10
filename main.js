c117337096d36fe1b7fdc65383fbbb2b31a99452
Sara Johnston <sarajohnston4@gmail.com>
2015-11-10 12:15:33 -0800

    dynamic dom creation in progress

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var goodreads_key = 'key: orU88aqllNy1ZoJLTTH9Q';
+var goodreads_secret = 'secret: 3fnOGoSE7vkY8s96FFeoppDlAiHXMGanqAORgCqwU8M';
 var global_result;
 
 $(document).ready(function () {
@@ -13,13 +15,21 @@ $(document).ready(function () {
                     var book_image = global_result.feed.entry[i]["im:image"][2].label;
                     var book_name = book_array[i]["im:name"].label;
                     var book_author = book_array[i]["im:artist"].label;
-
-                    $('img ').attr('src',book_image);
+                    var img_tag = $('<img>');
+                    var a_tag = $('<a>');
+                    var h3_tag = $('<h3>');
+                    var div_tag = $('<div>');
+                    $(a_tag).append(img_tag);
+                    $(h3_tag).append(a_tag);
+                    $(div_tag).append(h3_tag);
+                    $(div_tag).append(a_tag);
+                    $('.row1').append(div_tag);
+                    $(img_tag).attr('src',book_image);
                     $('.book_name').html(book_name);
                     $('p').html(book_author);
+
                     }
                 }
-
         });
         console.log('End of click function');
 });
